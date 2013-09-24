ed4657a3a07d4e14f943ed7a81626d37880432c6
George Lesica <george@lesica.com>
2013-09-24 01:06:45 -0600

    Generalize run script.

--- run.sh
+++ run.sh
@@ -1,23 +1,75 @@
-#!/bin/sh
+#!/bin/bash
 
-# Clean up from previous runs
-rm src/*.pyc
-mkdir -p output
-rm output/*.vtu
-rm output/*.txt
+#
+# Constants
+#
 
-# Read in parameters
-if [ -z "${1}" ]; then
-    echo "Error: number of timesteps ommitted" 1>&2
+PROGNAME=`basename $0`
+ 
+#
+# Helper Functions
+#
+
+# Error printer
+function error_echo() {
+    echo "${PROGNAME}: ${1:-"Unknown error"}" 1>&2
+}
+    
+# Error output function
+function error_exit() {
+    error_echo "${1}"
     exit 1
+} 
+
+# Prepare for a new run, dump old outputs, etc.
+function prepare_run() {
+    rm src/*.pyc
+    mkdir -p output
+    rm output/*.vtu
+    rm output/*.txt
+}
+
+# Cleanup after a run
+function teardown_run() {
+    rm src/*.pyc
+    rm output/*.txt
+}
+
+# Prints useful help information
+function help() {
+    echo "Usage: ${PROGNAME} <n>"
+    echo "    n : number of timesteps to run for"
+}
+
+#
+# Primary Functions
+#
+
+function run_packer() {
+    prepare_run
+
+    # Run the packing simulation
+    mpirun -np 2 `which esysparticle` src/packer.py ${NUM_TIMESTEPS}
+    dump2vtk -i output/pack-chk -o output/pack-vtk_ -t 0 ${NUM_SNAPSHOTS} 1000
+
+    teardown_run
+
+    return 0
+}
+
+#
+# Figure out what to run and do it
+#
+
+if [ -z "${2}" ]; then
+    help
+    error_exit "Error: number of timesteps ommitted"
 fi
-NUM_TIMESTEPS=${1}
+NUM_TIMESTEPS=${2}
 NUM_SNAPSHOTS=`expr $NUM_TIMESTEPS / 1000 + 1`
 
-# Run the packing simulation
-mpirun -np 2 `which esysparticle` src/packer.py ${NUM_TIMESTEPS}
-dump2vtk -i output/pack-chk -o output/pack-vtk_ -t 0 ${NUM_SNAPSHOTS} 1000
+case ${1} in
+    "packer" ) run_packer;;
+esac
 
-# Clean up after ourselves
-rm src/*.pyc
-rm output/*.txt
+exit ${?}
